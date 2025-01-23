f042cdad93e65bffe5ada8facef7e16acc68e967
Armand <armand.ayar@outlook.com>
2025-01-23 22:28:29 +0100

    almost done with partition

--- src/Quickhull.hs
+++ src/Quickhull.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE RebindableSyntax  #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use guards" #-}
+{-# HLINT ignore "Use zipWith" #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 
 module Quickhull (
@@ -23,7 +25,8 @@ module Quickhull (
 import Data.Array.Accelerate
 import Data.Array.Accelerate.Debug.Trace
 import qualified Prelude                      as P
-import Data.Array.Accelerate.Data.Maybe (justs)
+import Data.Array.Accelerate.Data.Maybe (isJust, maybe)
+import Data.Array.Accelerate.Smart
 
 
 -- Points and lines in two-dimensional space
@@ -74,7 +77,7 @@ pointIsOnLine (T2 (T2 x1 y1) (T2 x2 y2)) (T2 x y) = dy * x + dx * y1 - dy * x1 =
   dy = y2 - y1
 
 -- Make sure each True value is mapped to the right True index
--- for example: [True, False, False, True, True, False, True] -> [0, _, _, 1, 2, 3, _, 4]
+-- for example: [True, False, False, True, True, True, False, True] -> [0, _, _, 1, 2, 3, _, 4]
 boolToOffset :: Acc (Vector Bool) -> Acc (Vector Int)
 boolToOffset array = map (\i -> i - 1) $ scanl1 (+) arrayToInt where
     arrayToInt = map (\b -> if b then 1 else 0) array
@@ -95,40 +98,30 @@ initialPartition points =
       offsetUpper :: Acc (Vector Int)
       countUpper  :: Acc (Scalar Int)
       T2 offsetUpper countUpper = T2 offset count where
-        T2 _ count = filter (==expTrue) isUpper
+        count = unit $ (offset !! (length offset - 1)) + 1
         offset = boolToOffset isUpper
 
       offsetLower :: Acc (Vector Int)
-      countLower  :: Acc (Scalar Int)
-      T2 offsetLower countLower = T2 offset count where
-        T2 _ count = filter (==expTrue) isLower
-        offset = boolToOffset isLower
+      offsetLower = boolToOffset isLower
 
-      -- map each point to the index it will have in the new list, so that in can be thrown in a scatter function
-      destinations :: Acc (Vector (Maybe Int))
-      destinations = map mapper indexVector where
-        mapper index = if
-          point == p1 then Just_ 0 else if
-          point == p2 then Just_ $ the countUpper + 1 else if
-          isUpper !! index then Just_ ((offsetUpper !! index) + 1) else if
-          isLower !! index then Just_ (1 + the countUpper + (offsetLower !! index) + 1)
+      -- map each point to the index it will have in the new list, so that in can be thrown in a permute function
+      destinations :: Acc (Vector (Maybe DIM1))
+      destinations = map mapper (zip5 points isUpper isLower offsetLower offsetUpper) where
+        mapper :: Exp (Point, Bool, Bool, Int, Int) -> Exp (Maybe DIM1)
+        mapper (T5 point isPointUpper isPointLower offsetPointLower offsetPointUpper) = if
+          point == p1  then Just_ $ I1 0 else if
+          point == p2  then Just_ $ I1 (the countUpper + 1) else if
+          isPointUpper then Just_ $ I1 (offsetPointUpper + 1) else if
+          isPointLower then Just_ $ I1 (1 + the countUpper + offsetPointLower + 1)
           else Nothing_
-            where point = points !! index
-
-        shapePoints = shape points
-        indexVector = generate shapePoints indexHead
 
       newPoints :: Acc (Vector Point)
-      newPoints = scatter justDestinations defaultValue clearedPoints ++ p1vector where
-        -- the clearedPoints variable must have the same length as the justDestinations in order for scatter to work, 
-        -- thus filter out any point that has Nothing on its side
-        T2 clearedZippedPoints _ = filter (\(T2 _ destination) -> destination /= Nothing_) (zip points destinations)
-        clearedPoints = map fst clearedZippedPoints
-        T2 justDestinations _ = justs destinations
+      newPoints = permute const defaultValues (destinations !) points ++ p1vector where
         -- Vector (Z :. 1) [p1]
         p1vector = reshape (I1 1) (unit p1)
-        -- if everything goes alright, the default value should never be used, however the scatter function requires to have one
-        defaultValue = fill (shape clearedPoints) (T2 0 0) 
+        newSize = fold1 (+) $ map (\d -> if isJust d then 1 else 0) destinations :: Acc (Scalar Int)
+        -- if everything goes alright, the default value should never be used, however the permute function requires to have one
+        defaultValues = fill (I1 $ the newSize) undef
 
       headFlags :: Acc (Vector Bool)
       headFlags = map (\point ->  point == p1 || point == p2) newPoints
@@ -145,9 +138,66 @@ initialPartition points =
 -- p in that segment lies to the left of (p₁,p₃) or the right of (p₂,p₃).
 -- These points are undecided.
 --
+
+-- the same as boolToOffset, but then in segments
+boolToOffsetSegmented :: Acc (Vector Bool) -> Acc (Vector Bool) -> Acc (Vector Int)
+boolToOffsetSegmented headFlags array = map (\i -> i - 1) $ segmentedScanl1 (+) headFlags arrayToInt where
+    arrayToInt = map (\b -> if b then 1 else 0) array
+
 partition :: Acc SegmentedPoints -> Acc SegmentedPoints
 partition (T2 headFlags points) =
-  error "TODO: partition"
+  T2 newHeadFlags newPoints where
+    lines = zip (propagateR headFlags points) (propagateL headFlags points) :: Acc (Vector Line)
+    -- distances =                [_, (p0, 15), (p1, 10), (p2, 20), _, (p4, 6), (p5, 3), (p6, 8), (p7, 4), _]
+    distances = map (\(T2 line point) -> T2 point (nonNormalizedDistance line point)) (zip lines points) :: Acc (Vector (Point, Int))
+    -- furthestPoints = map fst $ [_, (p2, 20), (p2, 20), (p2, 20), _, (p6, 8), (p6, 8), (p6, 8), (p6, 8), _]
+    furthestPoints = map fst
+            $ propagateR headFlags
+            $ segmentedScanl1 (\i1@(T2 _ d1) i2@(T2 _ d2) -> if d1 > d2 then i1 else i2) headFlags distances :: Acc (Vector Point)
+
+    pointData = zip3 lines furthestPoints points :: Acc (Vector (Line, Point, Point))
+    isLeftFirstSegment =  map (\(T3 (T2 p1 _) p3 point) -> pointIsLeftOfLine (T2 p1 p3) point) pointData
+    isLeftSecondSegment = map (\(T3 (T2 _ p2) p3 point) -> pointIsLeftOfLine (T2 p3 p2) point) pointData
+
+    offsetLeftFirst :: Acc (Vector Int)
+    countLeftFirst  :: Acc (Vector Int)
+    T2 offsetLeftFirst countLeftFirst = T2 offset count where
+        count = map (+1) $ propagateR (shiftHeadFlagsL headFlags) offset
+        offset = boolToOffsetSegmented headFlags isLeftFirstSegment
+
+    offsetLeftSecond = boolToOffset isLeftSecondSegment :: Acc (Vector Int)
+
+    -- the relative index (not taking other segments into consideration) in the new vector, if there is any
+    destinations :: Acc (Vector (Maybe Int))
+    destinations = map mapper (zip6 pointData isLeftFirstSegment isLeftSecondSegment offsetLeftFirst offsetLeftSecond countLeftFirst) where
+        mapper :: Exp ((Line, Point, Point), Bool, Bool, Int, Int, Int) -> Exp (Maybe Int)
+        mapper (T6 (T3 (T2 p1 _) p3 point) isPointLeftFst isPointLeftSnd offsetPointLeftFst offsetPointLeftSnd countLeftPointFst) = if
+          point == p1    then Just_ 0 else if
+          point == p3    then Just_ (countLeftPointFst + 1) else if
+          isPointLeftFst then Just_ (offsetPointLeftFst + 1) else if
+          isPointLeftSnd then Just_ (1 + countLeftPointFst + offsetPointLeftSnd + 1)
+          else Nothing_
+
+    indexScan, indexOffset :: Acc (Vector Int)
+    indexScan = map (\n -> n - 1) $ scanl1 (+) $ map (\d -> if isJust d then 1 else 0) destinations
+    indexOffset = propagateL headFlags indexScan
+
+    realDestinations :: Acc (Vector (Maybe DIM1))
+    realDestinations = map
+        (\(T2 destination offset) -> maybe Nothing_ (\n -> Just_ $ I1 (n + offset)) destination)
+        (zip destinations indexOffset)
+
+    newPoints :: Acc (Vector Point)
+    newPoints = permute const defaultValues (realDestinations !) points where
+        newSize = the (fold1 (\_ b -> b) indexScan) - 1 :: Exp Int
+        -- if everything goes alright, the default value should never be used, however the permute function requires to have one
+        defaultValues = fill (I1 newSize) undef
+
+    newHeadFlags :: Acc (Vector Bool) 
+    newHeadFlags = undefined
+
+
+
 
 -- The completed algorithm repeatedly partitions the points until there are
 -- no undecided points remaining. What remains is the convex hull.
@@ -166,10 +216,10 @@ quickhull points =
 -- ----------------
 
 propagateL :: Elt a => Acc (Vector Bool) -> Acc (Vector a) -> Acc (Vector a)
-propagateL headFlags values = segmentedScanl1 (\_ b -> b) headFlags values
+propagateL = segmentedScanl1 const
 
 propagateR :: Elt a => Acc (Vector Bool) -> Acc (Vector a) -> Acc (Vector a)
-propagateR headFlags values = segmentedScanr1 (\_ b -> b) headFlags values
+propagateR = segmentedScanr1 const
 
 shiftHeadFlagsL :: Acc (Vector Bool) -> Acc (Vector Bool)
 shiftHeadFlagsL vector = scanr const expTrue (tail vector)
@@ -181,7 +231,7 @@ segmentedScanl1 :: Elt a => (Exp a -> Exp a -> Exp a) -> Acc (Vector Bool) -> Ac
 segmentedScanl1 f headFlags values = map snd (scanl1 (segmented f) (zip headFlags values))
 
 segmentedScanr1 :: Elt a => (Exp a -> Exp a -> Exp a) -> Acc (Vector Bool) -> Acc (Vector a) -> Acc (Vector a)
-segmentedScanr1 f headflags values = map snd (scanr1 (segmented f) (zip headFlags values))
+segmentedScanr1 f headFlags values = map snd (scanr1 (flip $ segmented f) (zip headFlags values))
 
 
 
